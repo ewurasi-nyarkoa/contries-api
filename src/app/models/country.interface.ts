9718b1b4d794f07dcb37b788223b1146f0588176
ewurasi nyarkoa <anewurasi@gmail.com>
2025-07-01 15:57:23 +0000

    refacted service to fetch from api and set up my interface to match the api

---
+++ src/app/models/country.interface.ts
@@ -0,0 +1,14 @@
+export interface Country {
+  flags: {
+    png: string;
+    svg: string;
+    alt?: string;
+  };
+  name: {
+    common: string;
+    official: string;
+  };
+  capital: string[];
+  region: string;
+  population: number;
+}
